d5adda7013a0e09fff9556176b07c99215ddc13f
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-16 15:18:50 +0100

    added redux with reducers and store, with each index.js

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,19 @@
+const initialState = {
+    favorites: {
+        content: []
+    }
+
+}
+
+const mainReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+
+
+}
+
+export default mainReducer 
